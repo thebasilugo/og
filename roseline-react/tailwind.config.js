ecc6752d366fefb0e5caa1ac5edf2f8d8e846bb3
Basil Ugo <basilugo2@gmail.com>
2024-10-10 01:33:12 +0100

    init tailwindconfig

---
+++ roseline-react/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				"deep-blue": "#003366",
+				teal: "#008080",
+			},
+		},
+	},
+	plugins: [],
+};
