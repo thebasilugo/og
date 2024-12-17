87faf5b4a6bd5cdca652e389ae5ac468acb7bc8b
Basil Ugo <basilugo2@gmail.com>
2024-12-17 10:00:07 +0100

    footer

---
+++ next/src/app/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+	return (
+		<footer className="bg-deep-blue text-gray-200 py-6">
+			<div className="container mx-auto px-4 text-center">
+				<p>
+					&copy; {new Date().getFullYear()} Dr. Roseline Oluwaseun Ogundokun.
+					All rights reserved.
+				</p>
+			</div>
+		</footer>
+	);
+}
